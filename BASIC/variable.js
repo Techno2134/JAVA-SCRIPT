41c38a9ceb3959b9ab3edf3ee732aada2a71bfc4
Techno2134 <137487665+Techno2134@users.noreply.github.com>
2025-06-23 18:52:44 +0000

    variable

---
+++ BASIC/variable.js
@@ -0,0 +1,13 @@
+const accountId = 131244;
+let accountEmail = 'ritugoyal@gmail.com';
+var accountPassword = '123445';
+accountCity = 'Mathura';
+let accountState;
+
+accountEmail = 'whdiuwehd';
+accountPassword = 'gdwegdyu';
+accountCity = 'delhi';
+
+console.log(accountEmail);
+console.log(accountPassword);
+console.table([accountEmail, accountId, accountPassword, accountCity, accountState]) 
