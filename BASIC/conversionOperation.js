ffa04e840070766467c516445ad739fc2ea5708c
Techno2134 <137487665+Techno2134@users.noreply.github.com>
2025-06-24 13:12:16 +0000

    conversion operation

--- BASIC/conversionOperation.js
+++ BASIC/conversionOperation.js
@@ -22,3 +22,52 @@ let someNumber = 33;
 let stringNum = String(someNumber);
 console.log(typeof someNumber);
 console.log(typeof stringNum);
+
+
+// <---------------operations---------------->
+let value = 3;
+let negValue = -value;
+console.log(negValue);
+
+let str1 = 'hy';
+let str2 = 'hy';
+let str3 = str1+str2;
+console.log(str3);
+
+console.log('1' + 2);
+console.log(1 + '2');
+console.log('1' + (2 + 2)); //use paranthesis.
+console.log((1 + 2) + '3');
+
+
+//not prefered
+console.log(true); //true
+console.log(+true); //1
+// console.log(true+) - possible hhi nhi h
+console.log(+""); //0
+ 
+let num1, num2, num3;
+num1 = num2 = num3 = 2+2;
+
+let gameCounter = 100;
+++gameCounter;
+console.log(gameCounter);
+
+//++a and a++ differnce
+let x = 3;
+const y = x++;
+console.log(x, y); // 4, 3
+// console.log(y);
+
+let a = 3;
+const b = ++a;
+console.log(a, b); //4,  4
+
+//++variable only
+//prefix ++x ->
+let c,d=5;
+d = ++c; //mtlb phle c ki value change hogi then assign hogi d ko
+
+//sufix x++ -> phle assign ho jaayegi then change hogi
+//sufix x-- -> phle use hogi then change hogi
+//prefix --x -> phle decrese ho jaayegi then assign hogi
