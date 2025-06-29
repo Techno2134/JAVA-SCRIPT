b0504effe379e231eb28860b223f5efaa588f4ae
Techno2134 <137487665+Techno2134@users.noreply.github.com>
2025-06-29 09:17:01 +0000

    scope

--- 03_BASICS/scope.js
+++ 03_BASICS/scope.js
@@ -21,3 +21,51 @@ if(true){
 console.log('outer',a);//300 hai 
 
 //console and terminal mai gobal scope alag hote hai
+
+
+//nested scope
+
+function one(){
+    const userName = 'Ritu';
+
+    function two(){
+        const website = 'youtube';
+        console.log(userName); //can access
+    }
+    // console.log(website); - can access
+
+    two(); //ritu
+}
+
+// one(); //one ke baad hi two execute hoga
+//closer - andr ka function bhar ke function ke excess kr pta hai
+if(true){
+    const userName = 'hitesh';
+    if(userName === 'hitesh'){
+        const website = 'fackbook';
+        console.log(userName + website);
+        
+    }
+    // console.log(website); //can't access
+}
+
+// console.log(userName); //can't access
+
+
+//-----------interesting-----------
+//basic function
+
+addOne(6); //np
+function addOne(num){
+    return num+1;
+}
+
+console.log(addOne(6));
+
+//expression
+// addTwo(); error because yhe hoisting nhi krte hai
+const addTwo = function(num){
+    return num+2;
+}
+
+console.log(addTwo(9)); 
