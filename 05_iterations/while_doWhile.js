dc9d3a5b721041e6fef5238e40d735442993d7be
Techno2134 <137487665+Techno2134@users.noreply.github.com>
2025-07-08 11:23:31 +0000

    while and do while loop

---
+++ 05_iterations/while_doWhile.js
@@ -0,0 +1,23 @@
+// let i = 0;
+// while(i<=10){
+//     console.log(`Value of i is ${i}`);
+//     i += 2;
+// }
+
+let myArr = ['Flash', 'Batman', 'Superman'];
+let i = 0;
+while(i < myArr.length){
+    console.log(`Value is ${myArr[i]}`)
+    i++;
+}
+
+//do while loop
+// do{
+// }while(condition);
+
+let score = 11;
+do{
+    console.log(`Score is ${score}`);
+    score++;
+}while(score <= 10);
+
